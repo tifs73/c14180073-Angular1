c9b61a23de442aa37aacc322b7d34e123a92aa4e
tifs73 <c14180073@john.petra.ac.id>
2021-02-27 10:38:29 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,19 @@ import { Component, VERSION } from "@angular/core";
 })
 export class AppComponent {
   name = "Angular " + VERSION.major;
-  test : String;
-  baris : 0;
-  kolom : 0;
-
-  hitung = [
-    ["*****", "*****", "*****", "*****", "*****"]
-  ];
+  public papan = new Array(5).fill(0).map(() => new Array(5).fill("*"));
+  test: String;
+  baris: 0;
+  kolom: 0;
+  counter = 0;
 
   button() {
     this.test = "berhasil";
+    if (this.counter % 2 == 0) {
+      this.counter++;
+      //player1 genap
+    } else {
+      //player2 ganjil
+    }
   }
 }
